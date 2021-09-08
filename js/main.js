9551b4db8932d66e48efcef16c6c4668f75c44c6
Meet Patel <mr.meetkpatel@gmail.com>
2021-09-07 18:11:30 -0700

    Done all changes

--- js/main.js
+++ js/main.js
@@ -1,25 +1,21 @@
 /* global data */
 /* exported data */
+
 function imgUrlInput(event) {
   var imgsrc = event.target.value;
   $imageDisplay.setAttribute('src', imgsrc);
 }
 function formJournalSubmit(event) {
-  var newObj = {};
   event.preventDefault();
+  var newObj = {};
   newObj.title = $formJournal.elements.title.value;
   newObj.imgsrc = $formJournal.elements.photoUrl.value;
   newObj.notes = $formJournal.elements.notes.value;
   newObj.entryId = data.nextEntryId;
-  data.nextEntryId = (data.nextEntryId) + 1;
-  data.entries.push(newObj);
-  var jsonEntries = JSON.stringify(data.entries);
-
-  localStorage.setItem('forms-entries', jsonEntries);
+  data.nextEntryId += 1;
+  data.entries.unshift(newObj);
   $imageDisplay.setAttribute('src', 'images/placeholder-image-square.jpg');
-  $formJournal.elements.title.value = '';
-  $formJournal.elements.photoUrl.value = '';
-  $formJournal.elements.notes.value = '';
+  $formJournal.reset();
 }
 var $imgurl = document.querySelector('#photoUrl');
 var $imageDisplay = document.querySelector('#imageDisplay');
