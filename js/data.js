1bf3bd28fa2e31471c2b4569171cc0984d6d2873
Meet Patel <mr.meetkpatel@gmail.com>
2021-09-08 17:08:52 -0700

    Working on the Ensure that submitting a new journal entry also creates a new DOM tree for it and adds it to the page.

--- js/data.js
+++ js/data.js
@@ -7,6 +7,11 @@ var data = {
   nextEntryId: 1
 };
 
+var previosData = localStorage.getItem('forms-journal-entries');
+if (previosData !== null) {
+  data = JSON.parse(previosData);
+}
+
 function beforeunloadFunction(event) {
   var jsonEntries = JSON.stringify(data);
   localStorage.setItem('forms-journal-entries', jsonEntries);
--- js/main.js
+++ js/main.js
@@ -23,3 +23,73 @@ var $formJournal = document.querySelector('#form-journal');
 // console.dir($imgurl);
 $imgurl.addEventListener('input', imgUrlInput);
 $formJournal.addEventListener('submit', formJournalSubmit);
+
+function domTree(entriesData) {
+  var $ul = document.createElement('ul');
+  var $li = document.createElement('li');
+  var $row = document.createElement('div');
+  var $columnHalfImg = document.createElement('div');
+  var $columnHalfText = document.createElement('div');
+  var $columnHalfTitle = document.createElement('div');
+  var $columnHalfPara = document.createElement('div');
+  var $imgElement = document.createElement('img');
+  var $titleH3 = document.createElement('h3');
+  var $para = document.createElement('p');
+
+  $row.setAttribute('class', 'row');
+  $columnHalfImg.setAttribute('class', 'column-half img-block');
+  $columnHalfText.setAttribute('class', 'column-half text-block');
+  $columnHalfTitle.setAttribute('class', 'column-half title-block');
+  $columnHalfPara.setAttribute('class', 'column-half para-block');
+  $imgElement.setAttribute('src', entriesData.imgsrc);
+
+  $ul.appendChild($li);
+  $li.appendChild($row);
+  $row.appendChild($columnHalfImg);
+  $columnHalfImg.appendChild($imgElement);
+  $row.appendChild($columnHalfText);
+  $columnHalfText.appendChild($columnHalfTitle);
+  $columnHalfTitle.appendChild($titleH3);
+  $columnHalfText.appendChild($columnHalfPara);
+  $columnHalfPara.appendChild($para);
+
+  $titleH3.textContent = entriesData.title;
+  $para.textContent = entriesData.notes;
+  return $ul;
+}
+var $displayEntriesDiv = document.querySelector('#displayEntries');
+var $newBtn = document.querySelector('.new-btn');
+var $tabs = document.querySelectorAll('.tab');
+var $entriesBtn = document.querySelector('.entriesBtn');
+
+$entriesBtn.addEventListener('click', entriesBtnClick);
+$newBtn.addEventListener('click', newBtnClick);
+
+function newBtnClick(event) {
+  for (var i = 0; i < $tabs.length; i++) {
+    if ($tabs[i].getAttribute('data-view') !== 'entries') {
+      $tabs[i].className = 'tab';
+    } else {
+      $tabs[i].className = 'tab hidden';
+    }
+  }
+}
+
+function entriesBtnClick(event) {
+  for (var i = 0; i < $tabs.length; i++) {
+    if ($tabs[i].getAttribute('data-view') !== 'entry-form') {
+      $tabs[i].className = 'tab';
+    } else {
+      $tabs[i].className = 'tab hidden';
+    }
+  }
+}
+
+function DOMContentLoadedCall(event) {
+  for (var i = 0; i < data.entries.length; i++) {
+    var $entriesFetch = domTree(data.entries[i]);
+    $displayEntriesDiv.appendChild($entriesFetch);
+  }
+}
+
+window.addEventListener('DOMContentLoaded', DOMContentLoadedCall);
