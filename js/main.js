bed3486f930522320d09a5fada52c10eed12989b
Meet Patel <mr.meetkpatel@gmail.com>
2021-09-08 21:03:19 -0700

    Completed issue-2-view-entries.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 /* global data */
 /* exported data */
+var $noEntry = document.querySelector('#noentry');
+var $displayEntriesDiv = document.querySelector('#displayEntries');
+var $imgurl = document.querySelector('#photoUrl');
+var $imageDisplay = document.querySelector('#imageDisplay');
+var $formJournal = document.querySelector('#form-journal');
+var $newBtn = document.querySelector('.new-btn');
+var $tabs = document.querySelectorAll('.tab');
+var $entriesBtn = document.querySelector('.entriesBtn');
 
 function imgUrlInput(event) {
   var imgsrc = event.target.value;
   $imageDisplay.setAttribute('src', imgsrc);
 }
+
 function formJournalSubmit(event) {
   event.preventDefault();
   var newObj = {};
@@ -16,11 +25,19 @@ function formJournalSubmit(event) {
   data.entries.unshift(newObj);
   $imageDisplay.setAttribute('src', 'images/placeholder-image-square.jpg');
   $formJournal.reset();
+  var $entriesFetch = domTree(newObj);
+  $displayEntriesDiv.prepend($entriesFetch);
+  for (var i = 0; i < $tabs.length; i++) {
+    if ($tabs[i].getAttribute('data-view') === data.view) {
+      $tabs[i].className = 'tab hidden';
+    } else {
+      $tabs[i].className = 'tab';
+      data.view = 'entries';
+    }
+  }
+  $noEntry.className = 'hidden';
 }
-var $imgurl = document.querySelector('#photoUrl');
-var $imageDisplay = document.querySelector('#imageDisplay');
-var $formJournal = document.querySelector('#form-journal');
-// console.dir($imgurl);
+
 $imgurl.addEventListener('input', imgUrlInput);
 $formJournal.addEventListener('submit', formJournalSubmit);
 
@@ -39,8 +56,8 @@ function domTree(entriesData) {
   $row.setAttribute('class', 'row');
   $columnHalfImg.setAttribute('class', 'column-half img-block');
   $columnHalfText.setAttribute('class', 'column-half text-block');
-  $columnHalfTitle.setAttribute('class', 'column-half title-block');
-  $columnHalfPara.setAttribute('class', 'column-half para-block');
+  $columnHalfTitle.setAttribute('class', 'column-half title-block width-100 ');
+  $columnHalfPara.setAttribute('class', 'column-half para-block width-100 ');
   $imgElement.setAttribute('src', entriesData.imgsrc);
 
   $ul.appendChild($li);
@@ -57,27 +74,15 @@ function domTree(entriesData) {
   $para.textContent = entriesData.notes;
   return $ul;
 }
-var $displayEntriesDiv = document.querySelector('#displayEntries');
-var $newBtn = document.querySelector('.new-btn');
-var $tabs = document.querySelectorAll('.tab');
-var $entriesBtn = document.querySelector('.entriesBtn');
 
-$entriesBtn.addEventListener('click', entriesBtnClick);
-$newBtn.addEventListener('click', newBtnClick);
+$entriesBtn.addEventListener('click', switchView);
+$newBtn.addEventListener('click', switchView);
 
-function newBtnClick(event) {
+function switchView(event) {
+  var viewSwitch = event.target.getAttribute('data-view');
+  data.view = viewSwitch;
   for (var i = 0; i < $tabs.length; i++) {
-    if ($tabs[i].getAttribute('data-view') !== 'entries') {
-      $tabs[i].className = 'tab';
-    } else {
-      $tabs[i].className = 'tab hidden';
-    }
-  }
-}
-
-function entriesBtnClick(event) {
-  for (var i = 0; i < $tabs.length; i++) {
-    if ($tabs[i].getAttribute('data-view') !== 'entry-form') {
+    if ($tabs[i].getAttribute('data-view') === viewSwitch) {
       $tabs[i].className = 'tab';
     } else {
       $tabs[i].className = 'tab hidden';
@@ -86,10 +91,24 @@ function entriesBtnClick(event) {
 }
 
 function DOMContentLoadedCall(event) {
-  for (var i = 0; i < data.entries.length; i++) {
-    var $entriesFetch = domTree(data.entries[i]);
-    $displayEntriesDiv.appendChild($entriesFetch);
+  if (data.entries.length !== 0) {
+    $noEntry.className = 'hidden';
+    for (var i = 0; i < data.entries.length; i++) {
+      var $entriesFetch = domTree(data.entries[i]);
+      $displayEntriesDiv.appendChild($entriesFetch);
+    }
+  } else {
+    $noEntry.className = 'shows';
   }
+
 }
 
 window.addEventListener('DOMContentLoaded', DOMContentLoadedCall);
+
+for (var i = 0; i < $tabs.length; i++) {
+  if ($tabs[i].getAttribute('data-view') === data.view) {
+    $tabs[i].className = 'tab';
+  } else {
+    $tabs[i].className = 'tab hidden';
+  }
+}
